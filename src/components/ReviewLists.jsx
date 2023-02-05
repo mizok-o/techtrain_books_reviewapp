4423db49851dd43b26fe85b12226b52c0cff707c
mizok-o <mizoko320@gmail.com>
2023-02-06 01:06:35 +0900

    テスト

--- src/components/ReviewLists.jsx
+++ src/components/ReviewLists.jsx
@@ -1,9 +1,9 @@
-import axios from 'axios'
-import { useEffect, useState } from 'react';
-import '../style/reviewList.css'
+import axios from 'axios';
+import {  useEffect, useState } from 'react';
+import '../style/reviewList.css';
 
 function Login() {
-  const [booksData, setBooksData] = useState([])
+  const [booksData, setBooksData ] = useState([])
   useEffect(() => {
     axios.get('https://api-for-missions-and-railways.herokuapp.com/public/books')
       .then((res) => {
@@ -11,15 +11,14 @@ function Login() {
       })
   })
 
-  const booksList = booksData.map((item, i) => {
+  const booksList = booksData.map((item , i) => {
     return (
-      <li className="review__item" key={i}>
-        <h3 className="review__item__title">{item.title}</h3>
-        <p className="review__item__desc">{item.detail}</p>
-        <p className="review__item__review">{item.review}</p>
-        <button className="review__item__btn">
-          <a href={item.url} className="review__item__link">購入</a>
-        </button>
+      <li className="review__item" key={i }>
+        <h3 className="review__item__title">{item.title } </h3>
+    <p className="review__item__desc">{item.detail}</p><p className="review__item__review">{item.review}</p>
+    <button className="review__item__btn">
+      <a href={item.url} className="review__item__link">購入</a>
+    </button>
       </li>
     )
   })
